b46d0329fac1bdd11c52a1e842035405ed5aab41
fzemi <filipzem11@gmail.com>
2024-05-22 15:47:04 +0200

    Initial commit

---
+++ src/main/java/com/fzemi/sscmaster/SscMasterApplication.java
@@ -0,0 +1,13 @@
+package com.fzemi.sscmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SscMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fzemi/sscmaster/SscMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzemi.sscmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
